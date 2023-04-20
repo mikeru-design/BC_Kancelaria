d34e3a066851f680f52c6348ca902cefb4a84df1
miekru <sajna.michal@gmail.com>
2023-04-20 11:27:53 +0200

    Add header section

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import './assets/scss/main.scss';
 import './assets/scss/navbar.scss';
+import './assets/scss/header.scss';
 import './assets/scss/_settings.scss';
 
 import Navbar from './assets/js/navbar';
