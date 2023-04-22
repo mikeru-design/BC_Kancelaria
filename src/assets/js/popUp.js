6f228f93ec5f8624ad25b3827b6de936677464a4
miekru <sajna.michal@gmail.com>
2023-04-23 00:24:56 +0200

    Add popUpWindow functionality in services

---
+++ src/assets/js/popUp.js
@@ -0,0 +1,48 @@
+import '../scss/popUp.scss'
+
+export default class PopUpWindow{
+  constructor(element){
+    this.element = element;
+  }
+
+  initPopUp(){
+    this.element.addEventListener('click', (e) => {
+      this.createWindow();
+      document.addEventListener('click', this.closeWindow)
+    })
+  }
+
+  createWindow(){
+    const popUpWindow = document.querySelector('.popUpWindow');
+    const description = this.element.querySelector('.description').outerHTML;
+
+    if ( !popUpWindow ){
+
+        const popUpWindow = document.createElement('div');
+        popUpWindow.classList.add('popUpWindow');
+
+        setTimeout( () => { popUpWindow.classList.add('showAfter') }, 100)
+
+        const popUpWindowClose = document.createElement('div');
+        popUpWindowClose.classList.add('popUpWindowClose');
+
+        const popUpWindowShadow = document.createElement('div');
+        popUpWindowShadow.classList.add('popUpWindowShadow');
+
+        popUpWindow.innerHTML = description;
+        popUpWindow.appendChild(popUpWindowClose);
+        document.body.appendChild(popUpWindow);
+        document.body.appendChild(popUpWindowShadow);
+    }
+  }
+
+  closeWindow(e){
+    const popUpWindow = document.querySelector('.popUpWindow');
+    const popUpWindowShadow = document.querySelector('.popUpWindowShadow');
+    if ( popUpWindow && e.target.classList.contains('popUpWindowClose')){
+      popUpWindow.remove();
+      popUpWindowShadow.remove();
+      document.removeEventListener('click', this.closeWindow)
+    }
+  }
+}
--- src/main.js
+++ src/main.js
@@ -4,11 +4,20 @@ import './assets/scss/navbar.scss';
 import './assets/scss/header.scss';
 import './assets/scss/aboutUs.scss';
 import './assets/scss/team.scss';
+import './assets/scss/services.scss';
 
 import Navbar from './assets/js/navbar';
 import showOnScroll from './assets/js/showOnScroll';
+import PopUpWindow from './assets/js/popUp';
 
 const navBar = new Navbar(document.querySelector('#navbar'));
 navBar.initAll();
+
+const popUpBtns = document.querySelectorAll('.card');
+
+popUpBtns.forEach(popUpBtn => {
+  popUpBtn = new PopUpWindow(popUpBtn);
+  popUpBtn.initPopUp();
+})
 
 window.addEventListener('scroll', showOnScroll);
